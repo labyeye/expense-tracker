d2fe88e7a8da2d1407d57f91e4f8bac8b6a3221f
Labh Bothra <labhbothra@Labhs-MacBook-Air.local>
2023-09-25 11:31:53 +0530

    Completed Login UI

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,13 +1,77 @@
 import React from "react";
-import { View,TextInput } from "react-native";
+import { Text,View,TextInput, StyleSheet, TouchableOpacity } from "react-native";
 
 const Login = () => {
     return(
-        <View>
-            <TextInput placeholder="Email"></TextInput>
-            <TextInput placeholder="Password"></TextInput>
+        <View style={styles.container}>
+            <View style={styles.detailbox}>
+                <View>
+                    <Text style={styles.logintext}>Login Here</Text>
+                </View>
+                <View style={styles.input}>
+                    <TextInput style={styles.textinput} placeholder="Email"/>
+                    <TextInput style={styles.textinput} placeholder="Password" />
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.loginbtn}>
+                        <Text style={styles.logintxt}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles= StyleSheet.create({
+    input:{
+        marginTop: 80,
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    container:{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#6ac1f7'
+
+    },
+    detailbox:{
+        flex: 0.5,
+        borderRadius: 10,
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'white'
+
+    },
+    textinput:{
+        height:50,
+        width:250,
+        margin: 10,
+        borderRadius: 10,
+        textAlign: 'center',
+        borderWidth: 3,
+        alignSelf: "center",
+        
+    },
+    logintext:{
+        fontWeight: "bold",
+        fontSize: 50,
+    },
+    loginbtn:{
+        width: 100,
+        height:45,
+        borderWidth: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        backgroundColor: 'white',
+    },
+    logintxt:{
+        color: 'black',
+    }
+})
 
 export default Login;
